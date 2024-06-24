d5b3b64435c7c0b79b1f968eaf74147186673a3d
Thien21166162 <21166162@st.hcmuaf.edu.vn>
2024-06-24 17:47:53 +0700

    init project

---
+++ myapp/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Contact
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        new_contact = Contact(name=name, email=email, message=message)
+        db.session.add(new_contact)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
